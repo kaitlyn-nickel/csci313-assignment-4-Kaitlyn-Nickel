a4739397fc56e30faeabd00533d74a1bf0c43e99
kaitlyn-nickel <nickelk102703@gmail.com>
2024-04-03 17:36:17 -0500

    Part 5: Created home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    #part5 challenge
+    num_book_harry = Book.objects.filter(title__contains='harry').count()
+    num_genre_word = Genre.objects.filter(name__contains = 'fiction').count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_book_harry': num_book_harry,
+        'num_genre_word': num_genre_word,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
